aa60fd5ffa72deb3f43cc1c2c1f860142e468a09
RiTop26 <265939993+RiTop26@users.noreply.github.com>
2026-05-20 15:54:17 -0600

    Edit reverseString.js

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let arr = word.split("");
+    let wordReverse = arr.reverse();
+    
+    let newString = wordReverse.join("");
+    return newString;
 };
 
 // Do not edit below this line
