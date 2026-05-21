62932d09f8c492e277a2d746657696d32f5b99e1
RiTop26 <265939993+RiTop26@users.noreply.github.com>
2026-05-20 22:53:31 -0600

    Edit removeFromArray.js

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,6 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...theArrs) {
+    let newArray = arr.filter((word) => !theArrs.includes(word))
+    return newArray;
 };
 
 // Do not edit below this line
