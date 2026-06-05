0060b1182f178712278d7c5da61a0e62cc1fe56b
RiTop26 <265939993+RiTop26@users.noreply.github.com>
2026-06-05 00:13:30 -0600

    Edit sumAll.js

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if (a < 0 || b < 0 || a % 1 !== 0 || b % 1 !== 0 || typeof(a) !== "number" || 
+    typeof(b) !== "number") {
+        return "ERROR";
+    }
+    let theArr = [a, b]
+    let num = theArr.sort();
+    let small = num.at(0)
+    let large = num.at(1)
+    let sum = 0;
+    for (let i = small; i <= large; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
