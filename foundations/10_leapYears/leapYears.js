0513c667647e060f38a07251b7c14e7b9e131e55
RiTop26 <265939993+RiTop26@users.noreply.github.com>
2026-06-05 20:51:47 -0600

    Edit leapYears.js

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
+const leapYears = function(theYear) {
+    if ((theYear % 4 == 0 && theYear % 100 != 0) || theYear % 400 == 0) {
+        return true;
+    }
 
+    else {
+        return false;
+    }
 };
 
 // Do not edit below this line
