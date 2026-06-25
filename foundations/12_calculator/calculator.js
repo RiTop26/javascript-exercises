dde2b067c019274aa0f5c3660298d9652bf511f6
RiTop26 <265939993+RiTop26@users.noreply.github.com>
2026-06-24 18:04:36 -0600

    Solved the exercise 12_calculator

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let sumUp = 0;
+	for(let i = 0; i < arr.length; i++) {
+    sumUp += arr[i];
+  }
+  return sumUp;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let product = 1;
+  for(let i = 0; i < arr.length; i++) {
+    product *= arr[i];
+  }
+  return product;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(factorialCompute) {
+  let myFactorial = 1;
+	if(factorialCompute == 0 || factorialCompute == 1) {
+    return 1;
+  }
+  else {
+    for(let i = 1; i <= factorialCompute; i++) {
+      myFactorial *= i;
+    }
+    return myFactorial;
+  }
 };
 
 // Do not edit below this line
