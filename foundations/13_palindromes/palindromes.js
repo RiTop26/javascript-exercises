b3715d9df00cb71416e0f04eaffb474c044f17d3
RiTop26 <265939993+RiTop26@users.noreply.github.com>
2026-06-25 16:26:39 -0600

    Solved the exercises 13_palindromes

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,26 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    // Remove non-letter and non-number
+    const myStr = str.replace(/[^a-zA-Z0-9]/g, "");
 
+    // Convert myStr to lowercase
+    const theStrs = myStr.toLowerCase();
+
+    // Split theStrs 
+    let arr = theStrs.split("");
+
+    // Reverse arr
+    let strReverse = arr.reverse();
+
+    // Join strReverse
+    let newString = strReverse.join("");
+
+    // check if newString and theStrs are palindromes
+    if(theStrs == newString) {
+        return true;
+    }
+    else {
+        return false;
+    }
 };
 
 // Do not edit below this line
