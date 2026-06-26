8db5eaca7820423c13885e9aeea9c6ee615a9a82
RiTop26 <265939993+RiTop26@users.noreply.github.com>
2026-06-25 20:45:00 -0600

    Solved the exercise 14_fibonacci and added fibonacci.js

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    let prevNumber1 = 1;
+    let prevNumber2 = 1;
+    let currentNumber;
 
+    if(n == 0) {
+        return 0;
+    }
+    else if(n < 0){
+        return "OOPS";
+    }
+
+    else if(n == 1 || n == 2) {
+        return 1;
+    }
+
+    for (let i = 3; i <= n; i++) {
+        currentNumber = prevNumber1 + prevNumber2;
+        prevNumber1 = prevNumber2;
+        prevNumber2 = currentNumber;
+    }
+    return currentNumber;
 };
 
 // Do not edit below this line
