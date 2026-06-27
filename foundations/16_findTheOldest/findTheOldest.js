45e297afbae17da33262dfa618ed6b1926af3bb5
RiTop26 <265939993+RiTop26@users.noreply.github.com>
2026-06-26 20:44:42 -0600

    Solved the exercise 16_findTheOldest and added findTheOldest.js

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+const oldestPerson = people.reduce((oldest, current) => {
+    const oldestAge = (oldest.yearOfDeath || new Date().getFullYear()) - oldest.yearOfBirth;
+    const currentAge = (current.yearOfDeath || new Date().getFullYear())- current.yearOfBirth;
+    if(oldestAge > currentAge) {
+        return  oldest;
+    }
+    else {
+        return current;
+    }
+});
+return oldestPerson;
+    
 };
 
 // Do not edit below this line
